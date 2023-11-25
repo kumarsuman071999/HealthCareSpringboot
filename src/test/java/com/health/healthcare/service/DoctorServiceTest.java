122a6f99e0d84aa60aeb2fad2d758aecf65c84b1
suman kumar <kumarsuman071999@gmail.com>
2023-11-25 23:27:24 +0530

    change in test service

--- src/test/java/com/health/healthcare/service/DoctorServiceTest.java
+++ src/test/java/com/health/healthcare/service/DoctorServiceTest.java
@@ -5,12 +5,13 @@ import com.health.healthcare.repository.DoctorRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class DoctorServiceTest {
 
-    @Mock
+    @Autowired
     private DoctorRepository doctorRepository;
 
     @InjectMocks
